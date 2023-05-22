1222c0d755b72a8dbd056e60ab8db033f8d8d13f
ogutu333 <ogutu333@gmail.com>
2023-05-22 10:11:58 +0000

    Update

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
-""" Script that uses JSONPlaceholder API to get information about employee """
-import requests
-import sys
+"""
+returns information about employee TODO list progress based on id
+"""
+from requests import get
+from sys import argv
 
 
-if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/'
-
-    user = '{}users/{}'.format(url, sys.argv[1])
-    res = requests.get(user)
-    json_o = res.json()
-    print("Employee {} is done with tasks".format(json_o.get('name')), end="")
-
-    todos = '{}todos?userId={}'.format(url, sys.argv[1])
-    res = requests.get(todos)
-    tasks = res.json()
-    l_task = []
-    for task in tasks:
-        if task.get('completed') is True:
-            l_task.append(task)
-
-    print("({}/{}):".format(len(l_task), len(tasks)))
-    for task in l_task:
-        print("\t {}".format(task.get("title")))
+if __name__ == '__main__':
+        done = 0
+        tasks = []
+        root = 'https://jsonplaceholder.typicode.com'
+        name = get(root + '/users/{}'.format(argv[1])).json().get('name')
+        for i in get(root + '/todos').json():
+            if i.get('userId') == int(argv[1]):
+                tasks.append(i)
+                if i.get('completed') is True:
+                    done += 1
+        print("Employee {} is done with tasks({}/{}):".
+              format(name, done, len(tasks)))
+        for i in tasks:
+            if i.get('completed') is True:
+                print("\t {}".format(i.get('title')))
