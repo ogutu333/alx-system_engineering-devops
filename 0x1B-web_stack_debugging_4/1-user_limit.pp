b83b0d84dabee1923359bedab86ff60a43fb8ecb
ogutu333 <ogutu333@gmail.com>
2023-10-19 20:08:29 +0000

    Web stack debugging 4

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,3 +1,3 @@
-# Changes the OS configuration so that it is possible to login with
+# Changes the OS of the configuration so that it is possible to login with
 # the holberton user and open a file without any error message.
-exec { '/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.conf': }
+exec { '/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.config': }
